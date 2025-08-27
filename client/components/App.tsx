8cd113b3976b6e42e2eb2ee49d165556dcd44018
Kate Newton <katemarieenewton@gmail.com>
2025-08-28 10:55:40 +1200

    changing text

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,7 +3,7 @@ import Home from './Home.tsx'
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '6rem' }}>
-      <h1 style={{ fontSize: '3rem', fontWeight: 'bold' }}>Lard Lad Donuts 🍩 🩷</h1>
+      <h1 style={{ fontSize: '3rem', fontWeight: 'bold' }}>Lard Lad Donut Generator 🍩 🩷</h1>
       <Home />
     </div>
   )
