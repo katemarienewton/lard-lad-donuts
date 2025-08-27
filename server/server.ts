41a3da72c557077407cffc7c68b70a89488be9a3
Lucas Spilhaus <lucasspilhausnz@gmail.com>
2025-08-27 17:17:46 +1200

    Donut ingredients file added and filesystems setup

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import * as Path from 'node:path'
 import express from 'express'
+import donuts from './routes/donuts.ts'
 import cors, { CorsOptions } from 'cors'
 
 const server = express()
@@ -13,6 +14,7 @@ server.get('/api/v1/greeting', (req, res) => {
 
 server.use(express.json())
 server.use(cors('*' as CorsOptions))
+server.use('/api/v1/donuts', donuts)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
